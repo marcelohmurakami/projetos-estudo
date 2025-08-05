99beed9d4e76270828092bf7cc3eefd05604b77c
MarceloMurakami <marcelo.murakami100@gmail.com>
2025-08-05 17:24:46 -0300

    commit

--- wild-oasis/src/App.jsx
+++ wild-oasis/src/App.jsx
@@ -1,8 +1,26 @@
+import styled from 'styled-components';
+import Button from './ui/Button';
+import Input from './ui/Input';
+
+const StyledApp = styled.div `
+  padding: 3em;
+  background-color: red;
+`
+
+const H1 = styled.h1 `
+  font-size: 3em;
+  font-weight: bold;
+  background-color: yellow;
+`
+
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <StyledApp>
+      <H1>The-Wild-Oasis</H1>
+      <Button>Check-in</Button>
+      <Button>Check-out</Button>
+      <Input></Input>
+    </StyledApp>
   )
 }
 
---
+++ wild-oasis/src/ui/Input.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Input = styled.input `
+  border-radius: 5px;
+  outline: solid black 1px;
+  padding: .5em
+`
+
+export default Input;
